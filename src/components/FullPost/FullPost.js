f41113c89229657416bbfd1a48c4504bb4e83ec7
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-20 11:49:06 +0200

    finished video 8 from module 9

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
+    state = {
+        loadedPosts: null,
+    }
+
+    shouldComponentUpdate (nextProps, nextState) {
+        if ( !this.state.loadedPosts) {
+            return true;
+        }
+        if(this.props.selectedPostID !== nextProps.selectedPostID || this.state.loadedPosts.id !== nextProps.selectedPostID){
+            return true;
+        }
+        return false;
+    }
+
+    componentDidUpdate() {
         if(this.props.selectedPostID != null) {
+            axios.get("https://jsonplaceholder.typicode.com/posts/"+this.props.selectedPostID).then((response)=>this.setState({loadedPosts: response.data}));
+        }
+    }
+    render () {
+        let post = <p style = {{textAlign: 'center'}} >Please select a Post!</p>;
+        if(this.props.selectedPostID != null) {
+            post = <p style = {{textAlign: 'center'}} >Loading...!</p>;
+        }
+        if(this.state.loadedPosts != null) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPosts.title}</h1>
+                    <p>{this.state.loadedPosts.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
