0771d7fa4a0e78d2e159350cd6a7fdb96cf36723
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-23 03:43:08 +0200

    finished video 10 from module 11

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
 
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 
 class Blog extends Component {
     render () {
@@ -13,8 +13,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">HOME</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">HOME</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
