bada8afc5bdc1a66340b7adc750f3b9b5b0ef052
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-26 20:36:50 +0200

    finished video 15 from module 11

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import './Blog.css';
-import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+import React, { Component } from "react";
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
-import {Route, NavLink} from 'react-router-dom'
+import { Route, NavLink } from "react-router-dom";
 
 class Blog extends Component {
-    render () {
-        
+    render() {
         return (
-            <div className= "Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink to="/" exact>HOME</NavLink></li>
-                            <li><NavLink to="/new-post" exact>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render ={() => <Posts />} />
-                <Route path="/new-post" exact render ={() => <NewPost />} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-            </div>
+        <div className="Blog">
+            <header>
+            <nav>
+                <ul>
+                <li>
+                    <NavLink
+                    to="/"
+                    exact
+                    activeClassName="my-active"
+                    activeStyle={{ color: "red", textDecoration: "underline" }}
+                    >
+                    HOME
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/new-post" exact>
+                    New Post
+                    </NavLink>
+                </li>
+                </ul>
+            </nav>
+            </header>
+            {/* <Route path="/" exact render ={() => <Posts />} />
+                    <Route path="/new-post" exact render ={() => <NewPost />} /> */}
+            <Route path="/" exact component={Posts} />
+            <Route path="/new-post" exact component={NewPost} />
+        </div>
         );
     }
 }
