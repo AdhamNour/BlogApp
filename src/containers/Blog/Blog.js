723592df9973929ca3f94625fef25e3696101ec2
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-01-27 06:25:21 +0200

    finished video 19 from module 11

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import FullPost from './FullPost/FullPost'
 
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink,Switch } from "react-router-dom";
 
 class Blog extends Component {
     render() {
@@ -31,11 +31,11 @@ class Blog extends Component {
                 </ul>
             </nav>
             </header>
-            {/* <Route path="/" exact render ={() => <Posts />} />
-                    <Route path="/new-post" exact render ={() => <NewPost />} /> */}
-            <Route path="/" exact component={Posts} />
-            <Route path="/new-post" exact component={NewPost} />
-            <Route path="/:id" exact component={FullPost} />
+            <Switch>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
+            </Switch>
         </div>
         );
     }
