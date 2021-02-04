94961b72482f4cc83ea043179498567cc02901ed
AdhamNour <adhamnourelwaffaa@gmail.com>
2021-02-03 23:57:46 -0500

    finished video 29 from module 11

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncCompenet"
 
 import { Route, NavLink,Switch, Redirect } from "react-router-dom";
 
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'))
+
 class Blog extends Component {
     state = {
-        auth : false
+        auth : true
     }
     render() {
         return (
@@ -34,7 +37,7 @@ class Blog extends Component {
             </nav>
             </header>
             <Switch>
-                {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
+                {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost} /> : null}
                 <Route path="/posts"  component={Posts} />
                 <Route render = {() => <h1>Not Found</h1>} />
                 {/* <Redirect  from="/" to="/posts"/> */}
