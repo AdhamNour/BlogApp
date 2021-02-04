c3738a89715334de2cfb8f69edad9c3e5494e1dc
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-02-04 04:38:37 +0200

    finished video 22 from module 11

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import FullPost from './FullPost/FullPost'
 
 import { Route, NavLink,Switch } from "react-router-dom";
 
@@ -15,7 +14,7 @@ class Blog extends Component {
                 <ul>
                 <li>
                     <NavLink
-                    to="/"
+                    to="/posts"
                     exact
                     activeClassName="my-active"
                     activeStyle={{ color: "red", textDecoration: "underline" }}
@@ -32,9 +31,8 @@ class Blog extends Component {
             </nav>
             </header>
             <Switch>
-                <Route path="/" exact component={Posts} />
                 <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Route path="/posts"  component={Posts} />
             </Switch>
         </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,62 +1,62 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import axios from '../../../axios'
-import Post from '../../../components/Post/Post';
-import './Posts.css'
-
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost"
+import "./Posts.css";
 
 class Posts extends Component {
-    state = {
-        posts: [],
-        selectedPostID: null,
-        erorr : false
-    };
-    componentDidMount () {
-        console.log(this.props)
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0,6);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author:"Adham Nour"
-                    }
-                })
-                this.setState({ posts: updatedPosts})
-            }).catch(error => {
-                //this.setState({ erorr: true })
-            });
-    }
+  state = {
+    posts: [],
+    selectedPostID: null,
+    erorr: false,
+  };
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 6);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Adham Nour",
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => {
+        //this.setState({ erorr: true })
+      });
+  }
 
-    selectPostHandler = (id) => {
-        this.props.history.push({pathname:'/'+id})
-    };
-    render() { 
-        let posts = this.state.posts.map(
-            post =>{
-                return (
-                    //<Link to={'/'+post.id} key={post.id}>
-                        <Post 
-                            title={post.title} 
-                            key={post.id}
-                            author={post.author} 
-                            onSelect={() => this.selectPostHandler(post.id)} 
-                        />
-                    //</Link>
-                )
-            }
-            )
-        if(this.state.erorr){
-            posts = <p>Some error with your internet connection</p>
-        } 
-        return (
-            
-            <section className="Posts">
-                    {posts}
-            </section>
-        );
+  selectPostHandler = (id) => {
+    this.props.history.push({ pathname: "/posts/" + id });
+  };
+  render() {
+    let posts = this.state.posts.map((post) => {
+      return (
+        //<Link to={'/'+post.id} key={post.id}>
+        <Post
+          title={post.title}
+          key={post.id}
+          author={post.author}
+          onSelect={() => this.selectPostHandler(post.id)}
+        />
+        //</Link>
+      );
+    });
+    if (this.state.erorr) {
+      posts = <p>Some error with your internet connection</p>;
     }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route path= {this.props.match.url +"/:id"} exact component={FullPost} />
+      </div>
+    );
+  }
 }
 
 export default Posts;
